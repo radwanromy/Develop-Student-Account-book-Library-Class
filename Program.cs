467e4429d4dcc436095fcbe0efac754198b54296
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2020-11-09 11:52:11 +0600

    Student Class Done commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Lab_6
 {
@@ -6,7 +10,17 @@ namespace Lab_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Student Information");
+            Console.WriteLine();
+            Console.WriteLine();
+            Student s = new Student("Radwan", "0001", "CSE",3);
+
+            s.ShowInfo();
+            Console.WriteLine();
+            Console.WriteLine("Customer and Account Information");
+            Console.WriteLine();
+            Console.WriteLine();
+
         }
     }
 }
--- Student.cs
+++ Student.cs
@@ -1,10 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Lab_6
 {
     class Student
     {
+        string name;
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        private string id;
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private float cgpa;
+
+        public float Cgpa
+        {
+            get { return cgpa; }
+            set { cgpa = value; }
+        }
+        private string department;
+
+        public string Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+        
+        public Student()
+        {
+            Console.WriteLine("Student Default");
+        }
+        public Student(string name, string id, string department, float cgpa)
+        {
+            this.name = name;
+            this.id = id;
+            this.department = department;
+            this.cgpa = cgpa;
+            Console.WriteLine("Student 1 parameter");
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Department: " + department);
+            Console.WriteLine("CGPA : " + cgpa);
+        }
+
     }
 }
